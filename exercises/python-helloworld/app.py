d5698656766a3ce603cf22d160e5e1e8d9a2f74a
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-08-22 06:55:50 +0200

    added status and metrics methods

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,23 @@
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def status():
+    return app.response_class(response=json.dumps({"result": "OK - healthy"}),status=200,mimetype='application/json')
+
+@app.route("/metrics")
+def metrics():
+    return app.response_class(response=json.dumps({"data": {"UserCount": "140", "UserCountActive": "23"}}),
+                              status=200,mimetype='application/json')
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
